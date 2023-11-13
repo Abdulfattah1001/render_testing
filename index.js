4ef38cc01b45b92638cc29b6e10f1cbd9bcbdb07
Abdulfattah <aminufattah6@gmail.com>
2023-11-13 10:20:54 +0100

    updated

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 3001;
 
 app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname,"public", "index.html"));
-    //res.send(`${path.join(__dirname, "public", "index.html")}`+"---------"+`${__dirname}`);
 })
 
 
